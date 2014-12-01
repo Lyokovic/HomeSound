722f8f7a4408d4491cd4d030e06bf02fe5cd20bf
victor <x>
2014-12-01 03:09:14 +0000

    Fix init bug of speakers states + init with old states values

--- homeSound/speakers.py
+++ homeSound/speakers.py
@@ -8,10 +8,11 @@ class Speakers:
         self.r = redis.StrictRedis()
 
         wiringpi.wiringPiSetup()
-        speakers = [x.split('/')[2] for x in self.r.keys('/device/*/audioPin')]
-        for pin in speakers:
+        # Get tuples (pin,state) of available speakers then initialize them
+        speakers = [(self.r.get('/device/'+x.split('/')[2]+'/audioPin'), self.r.get('/device/'+x.split('/')[2]+'/enabled')) for x in self.r.keys('/device/*/name')]
+        for (pin, state) in speakers:
             wiringpi.pinMode(int(pin),1)
-            wiringpi.digitalWrite(int(pin),1)
+            wiringpi.digitalWrite(int(pin),1-int(state))
 
 
     def enable(self,id):
